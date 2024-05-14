2ae776dc3a43d63183b59cbdee26e7b8a2271578
Arctan <72929040+ArtanBajqinca@users.noreply.github.com>
2024-05-14 10:59:19 +0200

    refactor: Update layout and styles
    - Update the title and description metadata in the layout.js file
    - Change the background color of the body in the layout.js file
    - Update the text color in the page.js file
    - Add a new Navbar component

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-   <h1 className="font-bold text-teal-500 text-2xl">
+   <h1 className="font-bold text-white text-2xl">
     Hello World
    </h1>
   );
