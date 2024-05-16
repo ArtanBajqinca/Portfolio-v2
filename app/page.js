0b5d4bdb36093d763c40a63b8a0d68e440dbce21
Arctan <artan.bajqinca@gmail.com>
2024-05-16 20:50:35 +0200

    chore: Update ProjectCard component to render dynamic data

--- app/page.js
+++ app/page.js
@@ -1,4 +1,7 @@
+import React from "react";
 import { ProjectCard } from "@/components/ProjectCard";
+import programmingProjects from "/data/programming-projecs.json";
+
 import "../public/fonts/Avenir-Light.ttf";
 import "../public/fonts/Avenir-Book.ttf";
 import "../public/fonts/Avenir-Roman.ttf";
@@ -8,12 +11,18 @@ import "../public/fonts/Avenir-Black.ttf";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center ">
-      <div className="w-full  flex flex-wrap gap-5 justify-center items-center ">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full flex flex-wrap gap-5 justify-center items-center">
+        {programmingProjects.map((project, index) => (
+          <ProjectCard
+            key={index}
+            title={project.title}
+            description={project.description}
+            imageSrc={project.imageSrc}
+            labels={project.labels}
+            link={project.link}
+          />
+        ))}
       </div>
     </div>
   );
--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,47 +1,56 @@
 import React from "react";
-import { Md10K, MdArrowOutward } from "react-icons/md";
+import { MdArrowOutward } from "react-icons/md";
 import { Label } from "@/components/Label";
+import Link from "next/link";
 
-export const ProjectCard = () => {
+export const ProjectCard = ({
+  title = "Coinnect Four",
+  description = "A multiplayer Connect Four game with a unique coin theme, developed for my Android course final project.",
+  imageSrc = "/images/programming-projects/Dummy.png",
+  labels = [],
+  link = "#",
+}) => {
   return (
-    <div className="flex bg-gray-700 w-[550px] h-[170px] rounded-[10px] group duration-200 transform transition-transform hover:scale-[102%]">
-      {/* Image */}
-      <div className="bg-lime-700 w-full m-4 rounded-[10px]">
-        <img
-          className="rounded-[10px]"
-          src="/images/programming-projects/Dummy.png"
-          alt="dummy image"
-        />
+    <Link href={link} passHref>
+      <div className="flex bg-gray-700 w-[550px] rounded-[10px] group duration-200 transform transition-transform hover:scale-[102%] cursor-pointer">
+        {/* Image */}
+        <div className="bg-lime-700 w-full m-3 rounded-[10px] flex">
+          <img
+            className="rounded-[10px] border-2 border-gray-600 object-cover w-full"
+            src={imageSrc}
+            alt={`${title} project screenshot`}
+          />
+        </div>
+        <div className="w-full my-3 mr-3">
+          {/* Title and arrow */}
+          <div className="w-full flex justify-between">
+            <div className="text-white text-[20px] font-avenir-heavy">
+              {title}
+            </div>
+            <div className="group-hover:translate-x-[7.5px] group-hover:-translate-y-1 transition-transform duration-200">
+              <MdArrowOutward
+                className="text-white"
+                size={20}
+                aria-label={`Open project ${title}`}
+              />
+            </div>
+          </div>
+          {/* Description */}
+          <div className="w-full flex items-end mt-1 mb-3">
+            <div className="text-white text-[14px] font-avenir-light leading-[16px]">
+              {description}
+            </div>
+          </div>
+          {/* Labels */}
+          <div className="w-full h-[30%]">
+            <div className="flex flex-wrap gap-1.5">
+              {labels.map((label, index) => (
+                <Label key={index} text={label} />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="w-full my-4 mr-4">
-        {/* Title and arrow */}
-        <div className="w-full flex justify-between">
-          <div className="text-white text-[20px] font-avenir-heavy">
-            Coinnect Four
-          </div>
-          <div className="group-hover:translate-x-[7.5px] group-hover:-translate-y-1 transition-transform duration-200">
-            <MdArrowOutward className="text-white" size={20} />
-          </div>
-        </div>
-        {/* Description */}
-        <div className="w-full flex items-end mt-1 mb-3">
-          <div className="text-white text-[14px] font-avenir-light leading-[16px]">
-            A multiplayer Connect Four game with a unique coin theme, developed
-            for my Android course final project.
-          </div>
-        </div>
-        {/* Labels */}
-        <div className="w-full h-[30%]">
-          <div className="flex flex-wrap gap-1.5">
-            <Label text="Android" />
-            <Label text="Kotlin" />
-            <Label text="Jetpack Compose" />
-            <Label text="Firebase" />
-            <Label text="Multiplayer" />
-            <Label text="App development" />
-          </div>
-        </div>
-      </div>
-    </div>
+    </Link>
   );
 };
