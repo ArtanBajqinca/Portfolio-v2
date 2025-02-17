ce0e925d592bd1b1520be09c6d8c4ae7efdfdac5
ArtanBajqinca <72929040+ArtanBajqinca@users.noreply.github.com>
2025-02-17 17:46:39 +0100

    feat: Update LabelBig and LabelIcon components for improved styling, modify project descriptions, and add new project preview images

--- components/LabelBig.jsx
+++ components/LabelBig.jsx
@@ -1,25 +1,26 @@
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export const LabelBig = ({ 
-  text, 
-  icon, 
-  link, 
-  bgColor = "bg-green-900", 
-  textColor = "text-green-600" 
+export const LabelBig = ({
+  text,
+  icon,
+  link,
+  bgColor = 'bg-green-900',
+  textColor = 'text-green-600',
 }) => {
   const LabelContent = () => (
-    <div className={`inline-flex px-3 py-1 pt-[6px] sm:pt-1 ${bgColor} ${textColor} font-avenir-medium rounded-full text-[14px] justify-center items-center`}>
-      {icon && (
-        typeof icon === "function" ? (
+    <div
+      className={`inline-flex px-3 py-1 pt-[6px] sm:pt-1 ${bgColor} ${textColor} font-avenir-medium rounded-full text-[14px] justify-center items-center`}
+    >
+      {icon &&
+        (typeof icon === 'function' ? (
           <icon className="mr-2" size={16} />
         ) : (
           <div className="mr-2">
             <Image src={icon} alt={text} width={16} height={16} />
           </div>
-        )
-      )}
+        ))}
       {text}
     </div>
   );
--- components/LabelIcon.jsx
+++ components/LabelIcon.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
 export const LabelIcon = ({ icon: Icon, label, bgColor, textColor }) => {
   return (
     <div
       className={`inline-flex px-2 py-[2px] max-h-[18px] mt-0.5 font-avenir-medium ${bgColor} ${textColor} rounded-full text-[10px] justify-center items-center`}
     >
-      {typeof Icon === "function" ? (
+      {typeof Icon === 'function' ? (
         <Icon className="mr-1" size={13} />
       ) : (
         <div className="mr-1">
           <Image src={Icon} alt={label} width={13} height={13} />
         </div>
       )}
-            <span style={{ marginTop: '0px' }}>{label}</span>
+      <span style={{ marginTop: '0px' }}>{label}</span>
     </div>
   );
 };
