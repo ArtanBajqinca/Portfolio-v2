d56b56499e8f15b7f378cb4eff33a58e9bb18039
ArtanBajqinca <72929040+ArtanBajqinca@users.noreply.github.com>
2025-03-13 12:00:23 +0100

    feat: Integrate Vercel Analytics into the layout component

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Artan Bajqinca - App Developer Portfolio',
@@ -9,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gray-700">{children}</body>
+      <body className="bg-gray-700">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
